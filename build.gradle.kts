79cc24b5056540e64af1c604d81ccd8b7ed0c823
Duncan Calvert <dcalvert@ihmc.us>
2018-09-27 18:54:47 -0500

    Add main plugin class.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,50 @@
+import com.gradle.publish.MavenCoordinates
+
 plugins {
+   `java-gradle-plugin`
    id("us.ihmc.ihmc-build") version "0.15.1"
+   id("com.gradle.plugin-publish") version "0.9.9"
 }
 
 ihmc {
    group = "us.ihmc"
    version = "0.0.1"
-   vcsUrl = "https://stash.ihmc.us/projects/LIBS/repos/log-tools"
+   vcsUrl = "https://github.com/ihmcrobotics/log-tools"
+   openSource = true
+   maintainer = "Duncan Calvert <dcalvert@ihmc.us>"
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+dependencies {
+   compile(gradleApi())
+}
+
+ihmc.sourceSetProject("api").dependencies {
+
+}
+
+gradlePlugin {
+   plugins {
+      register("logToolsPlugin") {
+         id = ihmc.group + "." + project.name
+         displayName = "Log Tools"
+         implementationClass = "us.ihmc.log.LogToolsPlugin"
+         description = "Message logging tools for IHMC Robotics."
+      }
+   }
+}
+
+pluginBundle {
+   website = "https://github.com/ihmcrobotics/log-tools"
+   vcsUrl = "https://github.com/ihmcrobotics/log-tools"
+   description = "Message logging tools for IHMC Robotics."
+   tags = listOf("log", "tools", "ihmc", "robotics")
+
+   mavenCoordinates(closureOf<MavenCoordinates> {
+      groupId = ihmc.group
+      artifactId = project.name
+      version = ihmc.version
+   })
 }
