8237c7e63224eab24a2f2151ad3feab080b2f457
Duncan Calvert <dcalvert@ihmc.us>
2018-09-27 16:57:49 -0500

    Build files.

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.15.1"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "0.0.1"
+   vcsUrl = "https://stash.ihmc.us/projects/LIBS/repos/log-tools"
+}
