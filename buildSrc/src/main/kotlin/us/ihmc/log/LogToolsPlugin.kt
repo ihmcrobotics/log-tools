b5ae1f84792147861fa7473d17555a6a0d1793c6
Duncan Calvert <dcalvert@ihmc.us>
2018-10-01 12:41:16 -0500

    Start crafting property loading.

--- buildSrc/src/main/kotlin/us/ihmc/log/LogToolsPlugin.kt
+++ buildSrc/src/main/kotlin/us/ihmc/log/LogToolsPlugin.kt
@@ -2,11 +2,41 @@ package us.ihmc.log
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import kotlin.reflect.KClass
 
-class LogToolsPlugin: Plugin<Project>
+class LogToolsPlugin : Plugin<Project>
 {
-   override fun apply(target: Project)
+   // meta class
+   data class PropertyDefinition<T : Any>(val name: String, val klass: KClass<T>)
+
+   // property definitions
+   val LOG_LEVEL = PropertyDefinition("logLevel", LogLevel::class)
+
+   // property value holders
+   class NotSet
+
+   data class LogLevel(val value: String)
+
+   override fun apply(project: Project)
    {
-      println("Helllo I'm a Kotlin buildSrc plugin!")
+      val logLevel = loadProperty(project, "logLevel")
+      if (logLevel is String)
+      {
+
+      }
+   }
+
+   fun loadProperty(project: Project, name: String): Any
+   {
+      if (project.properties.containsKey(name))
+      {
+         val property = project.properties[name]
+         if (property != null && property is String)
+         {
+            return property.trim().toLowerCase()
+         }
+      }
+
+      return NotSet()
    }
 }
