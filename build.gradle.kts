0e36680fad515f8f641d0cbcc8302801faaf597e
Duncan Calvert <dcalvert@ihmc.us>
2018-12-18 13:32:03 -0600

    :bookmark: 0.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.3.0"
+   version = "0.3.1"
    vcsUrl = "https://github.com/ihmcrobotics/log-tools"
    openSource = true
    maintainer = "Duncan Calvert <dcalvert@ihmc.us>"
