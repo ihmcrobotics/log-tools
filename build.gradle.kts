cf6453498b90247371febfa6a827e0ab10aa8626
Duncan Calvert <dcalvert@ihmc.us>
2019-07-26 11:42:14 -0500

    :arrow_up: ihmc-commons 0.26.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 ihmc.sourceSetProject("test").dependencies {
    implementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.0")
-   compile("us.ihmc:ihmc-commons-testing:0.26.3")
+   compile("us.ihmc:ihmc-commons-testing:0.26.6")
 }
 
 // test that custom JavaExec tasks receive the log level from Gradle properties
