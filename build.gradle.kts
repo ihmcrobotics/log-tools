c6ea540bef6ae38179f851cc5f15ab4deb233e44
Duncan Calvert <dcalvert@ihmc.us>
2019-10-21 10:28:39 -0500

    :arrow_up: 0.19.7 fix custom publish urls

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
    `java-library`
-   id("us.ihmc.ihmc-build") version "0.19.5"
+   id("us.ihmc.ihmc-build") version "0.19.7"
    id("us.ihmc.ihmc-ci") version "5.0"
    id("us.ihmc.ihmc-cd") version "1.7"
    id("us.ihmc.log-tools")
