bb6a1b64dcdc2c6e38f8b0d6e090249fd1fb8fb2
Duncan Calvert <dcalvert@ihmc.us>
2019-10-22 21:18:21 -0500

    :arrow_up: ihmc-ci 5.3, ihmc-cd 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 plugins {
    `java-library`
    id("us.ihmc.ihmc-build") version "0.19.7"
-   id("us.ihmc.ihmc-ci") version "5.0"
-   id("us.ihmc.ihmc-cd") version "1.7"
+   id("us.ihmc.ihmc-ci") version "5.3"
+   id("us.ihmc.ihmc-cd") version "1.8"
    id("us.ihmc.log-tools")
 }
 
