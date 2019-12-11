89771bdc212f7ea3874f056da2c228cb1b66a358
Duncan Calvert <dcalvert@ihmc.us>
2019-12-11 17:38:59 -0600

    Disable log4j2 shutdown hook with test.

--- src/test/java/us/ihmc/log/LogToolsTest.java
+++ src/test/java/us/ihmc/log/LogToolsTest.java
@@ -1,6 +1,8 @@
 package us.ihmc.log;
 
+import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.Test;
+import us.ihmc.commons.thread.ThreadTools;
 
 public class LogToolsTest
 {
@@ -12,6 +14,32 @@ public class LogToolsTest
       LogTools.info("Hello there");
       LogTools.debug("Hello there");
       LogTools.trace("Hello there");
+   }
+
+   @Test
+   public void testShutdown()
+   {
+      Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+         LogTools.error("Shutting down");
+         LogTools.warn("Shutting down");
+         LogTools.info("Shutting down");
+         LogTools.debug("Shutting down");
+         LogTools.trace("Shutting down");
+      }));
+
+      ThreadTools.startAThread(() ->
+      {
+         for (int i = 0; i < 5; i++)
+         {
+            LogTools.info("Sleeping...");
+            ThreadTools.sleepSeconds(1.0);
+         }
+      }, "SleepThread");
+
+      ThreadTools.sleepSeconds(2.0);
+//      LogManager.shutdown();
+      Runtime.getRuntime().exit(0);
+      ThreadTools.sleepSeconds(2.0);
    }
    
    @Test
