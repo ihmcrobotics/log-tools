b578f2e0ec66df46ba72849aa158642ed57b79f1
Duncan Calvert <dcalvert@ihmc.us>
2020-03-19 17:53:40 -0500

    :arrow_up: ihmc-commons 0.28.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 }
 
 ihmc.sourceSetProject("test").dependencies {
-   compile("us.ihmc:ihmc-commons-testing:0.28.1")
+   compile("us.ihmc:ihmc-commons-testing:0.28.2")
 }
 
 // test that custom JavaExec tasks receive the log level from Gradle properties
