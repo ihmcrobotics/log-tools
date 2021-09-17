f075752f640a54d33c6fc179ec34c7b8f713e6cb
Duncan Calvert <dcalvert@ihmc.us>
2021-09-17 17:37:17 -0500

    :arrow_up: ihmc-cd 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "7.4"
-   id("us.ihmc.ihmc-cd") version "1.20"
+   id("us.ihmc.ihmc-cd") version "1.21"
    id("us.ihmc.log-tools-plugin")
 }
 
