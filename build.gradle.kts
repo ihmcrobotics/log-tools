46aaa6b0b46a3b24fa8d8f5e74b0f7d932838247
Dexton Anderson <danderson@ihmc.org>
2023-07-31 11:53:07 -0500

    :arrow_up: ihmc-cd 1.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 plugins {
    id("us.ihmc.ihmc-build")
    id("us.ihmc.ihmc-ci") version "8.3"
-   id("us.ihmc.ihmc-cd") version "1.24"
+   id("us.ihmc.ihmc-cd") version "1.26"
    id("us.ihmc.log-tools-plugin")
 }
 
